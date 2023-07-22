3e6151770368293044da6e7077a13cbd2c7221ab
Kayo Ronald <kayoronald4321@gmail.com>
2023-07-22 16:11:42 +0000

    feat: adding bcrypt

--- main.go
+++ main.go
@@ -1,47 +1,78 @@
 package main
 
 import (
-	"time"
 
+	"github.com/KayoRonald/go-fiber-jwt-test/database"
+	"github.com/KayoRonald/go-fiber-jwt-test/middleware"
 	"github.com/KayoRonald/go-fiber-jwt-test/models"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
+
+	// "github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 )
+
 func main() {
 	app := fiber.New()
 
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://*, https://*",
-		AllowHeaders: "Origin, Content-Type, Accept",
-		AllowMethods: "GET, POST, PUT, DELETE",
-    AllowCredentials: false,
-	}))
-
-	app.Use(limiter.New(limiter.Config{
-		Max:               20,
-		Expiration:        30 * time.Second,
-		LimiterMiddleware: limiter.SlidingWindow{},
-	}))
-
+	app.Use(logger.New())
+	app.Use(middleware.CorsMiddleware())
+	app.Use(middleware.Limiter())
+  // Conect Database
+  database.ConnectDB()
+  // metrics
 	app.Get("/metrics", monitor.New(monitor.Config{Title: "Metrics My API"}))
-	
-  app.Get("/", func(c *fiber.Ctx) error {
-    c.Accepts("application/json")
-		return c.SendString("Hello, World!")
+	// Public ROUTER
+	app.Get("/", func(c *fiber.Ctx) error {
+		c.Accepts("application/json")
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"status":  "success",
+			"message": "Welcome to Golang, Fiber, and GORM. Router public",
+		})
 	})
-	app.Post("/sinup", func(c *fiber.Ctx) error {
-    c.Accepts("application/json")
-    user := new(models.User)
-    if err := c.BodyParser(user); err != nil {
-      return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-        "message": err.Error(),
-        "status":  "err",
-      })
-    }
-    return c.SendString("")
+	//Private ROUTER
+	app.Get("/me", func(c *fiber.Ctx) error {
+		c.Accepts("application/json")
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"status":  "success",
+			"message": "Welcome to router private",
+		})
 	})
 
+	app.Post("/sinup", func(c *fiber.Ctx) error {
+		c.Accepts("application/json")
+		user := new(models.User)
+		if err := c.BodyParser(user); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": err.Error(),
+				"status":  "err",
+			})
+		}
+		hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"message": err.Error(),
+				"status":  "err",
+			})
+		}
+    createUser := models.User{
+      Name: user.Name,
+      Email: user.Email,
+      Password: string(hash),
+    }
+    database.Database.Db.Create(&createUser)
+		return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+			"message": &createUser,
+			"status":  "sucess",
+		})
+	})
+
+	app.All("*", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "Not Found",
+			"status":  "err",
+		})
+	})
 	app.Listen(":3000")
 }
